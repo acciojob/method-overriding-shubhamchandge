d03d7ae49bdcbf62922856e966c6c7962a4bd38d
shubham chandge <115560962+shubhamchandge@users.noreply.github.com>
2023-01-28 11:28:06 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	
+		  B obj = new B();
+             obj.meth();
+  }
+	}
+
+
+class A{
+String meth(){return "Invoking method from class A";}
+  }
+  class B extends A{
+ String meth(){return "Method is overridden in Extendend class B";}
+}
+  
